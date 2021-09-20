e2954ecab7a63f161be6e1ec1ef9d6fa728634e2
Guilherme Vilabril <vilabril@gmail.com>
2021-09-20 17:00:58 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/vilabril/workshopmongo/config/Instantiation.java
+++ src/main/java/com/vilabril/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.vilabril.workshopmongo.domian.Post;
 import com.vilabril.workshopmongo.domian.User;
+import com.vilabril.workshopmongo.dto.AuthorDTO;
 import com.vilabril.workshopmongo.repository.PostRepository;
 import com.vilabril.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu Viagem","Vou Viajar para São Paulo!",maria);
-		Post post2 = new Post(null,sdf.parse("23/03/2018"), "BOM dia","Acordei feliz!",maria);
-		
 	    userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu Viagem","Vou Viajar para São Paulo!",new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("23/03/2018"), "BOM dia","Acordei feliz!",new AuthorDTO(maria));
+		
 	    postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
