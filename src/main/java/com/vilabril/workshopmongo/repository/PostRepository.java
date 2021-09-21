b8723ff2f91a366c8df423ecc0af05cd167f75d3
Guilherme Vilabril <vilabril@gmail.com>
2021-09-21 14:46:21 -0300

    Consulta simples com query methods

--- src/main/java/com/vilabril/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vilabril/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.vilabril.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vilabril.workshopmongo.domian.Post;
@@ -10,6 +11,9 @@ import com.vilabril.workshopmongo.domian.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String Text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String Text);
 	
 }
