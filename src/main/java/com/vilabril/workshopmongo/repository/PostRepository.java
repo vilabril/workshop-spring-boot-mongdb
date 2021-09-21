6f91ee84ac4f868bc6e3bbb21c70eb51f1b62adf
Guilherme Vilabril <vilabril@gmail.com>
2021-09-21 18:09:45 -0300

    mples com query methods

--- src/main/java/com/vilabril/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vilabril/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.vilabril.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,5 +16,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> searchTitle(String Text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String Text);
+	
+	@Query("{$and:[ {date: {$gte: ?1} }, { date: {$lte: ?2}},{$or: [{'title': { $regex: ?0, $options: 'i' } },{'body': { $regex: ?0, $options: 'i' } },{'comments.text': { $regex: ?0, $options: 'i' } }]}]}")
+	List<Post> fullSearch(String Text, Date minDate, Date maxDate);
 	
 }
